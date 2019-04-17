2517f2fe9053c798b7836bdbf8530dc45b18e2f6
Addison G <corrrosiveacid@gmail.com>
2019-04-17 16:10:20 +1000

    Completed 'artificial-rain' challenge

--- c/artificial-rain/artificial-rain.c
+++ c/artificial-rain/artificial-rain.c
@@ -1,25 +1,22 @@
-#include <stddef.h>
-
-int artificial_rain(const int *heights, size_t n) {
-  int bestSpread = 0;
-  printf("\n\n");
-  for (int i = 0; i < n; i++) {
-    printf("%d-", heights[i]);
-    int slope = heights[i], spread = 1;
-    for (int left = i - 1; left >= 0 && heights[left] <= slope; left--) { // Left
-      slope = heights[left];
-      spread++;
+int artificial_rain(const int *heights, int n) {
+  int height = heights[0], best = 0, flat = 0, down = 0, up = 0;
+  for (int i = 1; i < n; i++) {
+    if (height == heights[i]) { // Flat ( - )
+      flat++;
+      continue;
+    } else if (heights[i] > height) { // Uphill ( / )
+      best = best > down + flat ? best : down + flat;
+      up += flat + 1;
+      down = 0;
+    } else if (down) { // Downhill ( \ )
+      down += flat + 1;
+    } else { // Local Max ( /\ )
+      down = up + flat + 1;
+      up = 0;
     }
-    
-    slope = heights[i];
-    for (int right = i + 1; right < n && heights[right] <= slope; right++) { // Right
-      slope = heights[right];
-      spread++;
-    }
-    
-    if (spread > bestSpread) {
-      bestSpread = spread;
-    }
+    flat = 0;
+    height = heights[i];
   }
-  return bestSpread;
+  best = best > up + down + flat ? best : up + down + flat;
+  return best + 1;
 }
